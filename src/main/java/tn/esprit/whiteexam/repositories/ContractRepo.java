fa348573406e8500dd7d7b4c2ac47af360a460fe
Amira Matoussi <amiramatoussi310@gmail.com>
2024-04-16 14:22:26 +0100

    Finished with the repository layer

--- src/main/java/tn/esprit/whiteexam/repositories/ContractRepo.java
+++ src/main/java/tn/esprit/whiteexam/repositories/ContractRepo.java
@@ -1,7 +1,8 @@
 package tn.esprit.whiteexam.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import tn.esprit.whiteexam.entities.Contract;
-
+@Repository
 public interface ContractRepo extends JpaRepository<Contract,Long> {
 }
--- src/main/java/tn/esprit/whiteexam/repositories/UsineRepo.java
+++ src/main/java/tn/esprit/whiteexam/repositories/UsineRepo.java
@@ -1,7 +1,8 @@
 package tn.esprit.whiteexam.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import tn.esprit.whiteexam.entities.Usine;
-
+@Repository
 public interface UsineRepo extends JpaRepository<Usine,Long> {
 }
